488e6388bbd57457c048776da5b0e0405da52ffd
Fructuoso <victor@fructuoso.com.br>
2020-10-22 08:51:20 -0300

    Segregando IFormatterExtensions pois não é responsabilidade do IDIstributedCacheExtensions

--- src/Workbench.IDistributedCache.Extensions/IDistributedCacheExtensions.cs
+++ src/Workbench.IDistributedCache.Extensions/IDistributedCacheExtensions.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 using System;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
+using Workbench.IFormatter.Extensions;
 using Abstractions = Microsoft.Extensions.Caching.Distributed;
 
 namespace Workbench.IDistributedCache.Extensions
 {
     public static class IDistributedCacheExtensions
     {
-        public static IFormatter Formatter { get; set; } = new BinaryFormatter();
-
         public static TEntity GetOrCreate<TEntity>(this Abstractions.IDistributedCache cache, string key, Func<TEntity> predicate)
         {
             TEntity entity = cache.Get<TEntity>(key);
@@ -30,39 +26,15 @@ namespace Workbench.IDistributedCache.Extensions
 
             var binary = cache.Get(key);
 
-            if (binary != null) { result = binary.FromByteArray<TEntity>(); }
+            if (binary != null) { result = binary.Deserialize<TEntity>(); }
 
             return result;
         }
-
         public static void Set<TEntity>(this Abstractions.IDistributedCache cache, string key, TEntity entity)
         {
             var options = new DistributedCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(20));
-            var binary = entity.ToByteArray();
+            var binary = entity.Serialize();
             cache.Set(key, binary, options);
         }
-
-        private static byte[] ToByteArray<TEntity>(this TEntity entity)
-        {
-            if (entity == null) { return default; }
-
-            using (MemoryStream stream = new MemoryStream())
-            {
-                Formatter.Serialize(stream, entity);
-                return stream.ToArray();
-            }
-        }
-
-        private static TEntity FromByteArray<TEntity>(this byte[] data)
-        {
-            if (data == null) return default;
-
-            using (MemoryStream stream = new MemoryStream(data))
-            {
-                var obj = Formatter.Deserialize(stream);
-                return (TEntity)obj;
-            }
-        }
-
     }
 }
---
+++ src/Workbench.IFormatter.Extensions/IFormatterExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Serialization = System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Workbench.IFormatter.Extensions
+{
+
+    public static class IFormatterExtensions
+    {
+        public static Serialization.IFormatter DefaultFormatter { get; set; } = new BinaryFormatter();
+
+        public static byte[] Serialize<TEntity>(this TEntity entity) => entity.Serialize(DefaultFormatter);
+        public static byte[] Serialize<TEntity>(this TEntity entity, Serialization.IFormatter formatter)
+        {
+            if (entity == null) { return default; }
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, entity);
+                return stream.ToArray();
+            }
+        }
+
+        public static TEntity Deserialize<TEntity>(this byte[] data) => data.Deserialize<TEntity>(DefaultFormatter);
+        public static TEntity Deserialize<TEntity>(this byte[] data, Serialization.IFormatter formatter)
+        {
+            if (data == null) return default;
+
+            using (MemoryStream stream = new MemoryStream(data))
+            {
+                var obj = formatter.Deserialize(stream);
+                return (TEntity)obj;
+            }
+        }
+    }
+}
